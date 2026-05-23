7cabbcc85d902580a47b959bebc5e4892fac89eb
Shantanu Kumar <rajatmaurya34@gmail.com>
2026-05-23 01:41:29 +0100

    removed invalid include

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,6 +1,5 @@
 #include <catch2/catch_test_macros.hpp>
 #include <internal_use_only/config.hpp>
-#include <vulkan_rt/sample_library.hpp>
 
 TEST_CASE("configured project metadata is available")
 {
@@ -8,8 +7,3 @@ TEST_CASE("configured project metadata is available")
   CHECK(vulkan_rt::cmake::project_version.size() > 0);
 }
 
-TEST_CASE("sample constexpr code works")
-{
-  STATIC_REQUIRE(factorial_constexpr(0) == 1);
-  STATIC_REQUIRE(factorial_constexpr(5) == 120);
-}
