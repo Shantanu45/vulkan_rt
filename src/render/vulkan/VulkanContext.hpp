c3db4a80da5d24528132c1773ca49c5a124c26e9
Shantanu Kumar <rajatmaurya34@gmail.com>
2026-05-24 23:05:17 +0100

    reformat header

--- src/render/vulkan/VulkanContext.hpp
+++ src/render/vulkan/VulkanContext.hpp
@@ -1,14 +1,14 @@
-/*****************************************************************/ /**
-                                                                     * \file   VulkanContext.hpp
-                                                                     * \brief  VulkanContext
-                                                                     * VkInstance
-                                                                     * debug messenger
-                                                                     * VkSurfaceKHR
-                                                                     * Vulkan loader/validation setup
-                                                                     *
-                                                                     * \author Shantanu Kumar
-                                                                     * \date   May 2026
-                                                                     *********************************************************************/
+/*****************************************************************
+* \file   VulkanContext.hpp
+* \brief  VulkanContext
+* VkInstance
+* debug messenger
+* VkSurfaceKHR
+* Vulkan loader/validation setup
+*
+* \author Shantanu Kumar
+* \date   May 2026
+*********************************************************************/
 #pragma once
 
 namespace vulkan_rt::render {
